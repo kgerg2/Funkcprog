dd630ecfb13e168128a34f06380ed270a5b27d1c
kgerg2 <46970565+kgerg2@users.noreply.github.com>
2020-10-23 11:33:56 +0200

    1. Erlang házi feladat

--- Erlang/xor_cipher.erl
+++ Erlang/xor_cipher.erl
@@ -4,18 +4,24 @@
 toBitString(0) -> [];
 toBitString(A) -> [A rem 2 | toBitString(A div 2)].
 
-charToBitString(A) ->
-    Bits = toBitString(A),
-    Pad = [0 || _ <- lists:seq(1, 8 - length(Bits))],
-    lists:append(Bits, Pad).
+charToBitString(A) -> toBitString(A).
+    % The following lines produce and 8 long list, but it is not needed for this task.
+    % Bits = toBitString(A),
+    % Pad = [0 || _ <- lists:seq(1, 8 - length(Bits))],
+    % lists:append(Bits, Pad).
 
 bitStringToChar(A) -> lists:foldr(fun(X, Y) -> X + 2*Y end, 0, A).
 
 xOr([], A) -> A;
 xOr(A, []) -> A;
-xOr([A|AS], [B|BS]) -> [A bxor B | xOr(AS, BS)].
+xOr([A|As], [B|Bs]) -> [A bxor B | xOr(As, Bs)].
 
-cycle(L, N) -> lists:flatten(lists:duplicate(N div length(L), L), lists:sublist(L, N rem length(L))).
+% cycle(L, N) -> lists:flatten(lists:duplicate(N div length(L), L), lists:sublist(L, N rem length(L))).
+cycle(L, N) -> cycle([], N, L).
+
+cycle(_, 0, _) -> [];
+cycle([], N, [H|T]) -> [H | cycle(T, N-1, [H|T])];
+cycle([H|T], N, L) -> [H | cycle(T, N-1, L)].
 
 encrypt(Text, Key) ->
     Keys = cycle(Key, length(Text)),
@@ -23,16 +29,23 @@ encrypt(Text, Key) ->
 
 decrypt(Text, Key) -> encrypt(Text, Key).
 
-isCycledIn(A, B) -> lists:prefix(A, B) and (cycle(A, length(B)) == B).
+% isCycledIn(A, B) -> lists:prefix(A, B) andalso (cycle(A, length(B)) == B).
+isCycledIn(A, B) -> lists:prefix(A, B) andalso isCycledIn([], B, A).
+
+isCycledIn(_, [], _) -> true;
+isCycledIn([], [B|Bs], [A|As]) -> A == B andalso isCycledIn(As, Bs, [A|As]);
+isCycledIn([A|As], [B|Bs], L) -> A == B andalso isCycledIn(As, Bs, L).
 
 getKey(Text, Cipher) -> 
     Keys = encrypt(Text, Cipher),
     Prefixes = [lists:sublist(Keys, N) || N <- lists:seq(1, length(Keys))],
-    {value, Key} = lists:search(fun(X) -> isCycledIn(X, Keys) end, Prefixes),
+    {value, Key} = lists:search(fun(X) -> isCycledIn(X, Keys) end, Prefixes), % cannot fail, last element is the whole list
     Key.
 
 decodeMessage(Cipher, PartOfText) ->
     KeyFirst = getKey(PartOfText, Cipher),
-    {N, KeyShifted} = lists:foldr(fun({N, X}, {M, Y}) -> if length(X) < length(Y) -> {N, X}; true -> {M, Y} end end, {0, KeyFirst}, [{N-1, getKey(PartOfText, lists:sublist(Cipher, N, length(PartOfText)))} || N <- lists:seq(2, length(Cipher) - length(PartOfText) + 1)]),
-    Key = lists:concat([lists:sublist(KeyShifted, length(KeyShifted) - (N rem length(KeyShifted)) + 1, N rem length(KeyShifted)), lists:sublist(KeyShifted, length(KeyShifted) - (N rem length(KeyShifted)))]),
+    Keys = [{N-1, getKey(PartOfText, lists:sublist(Cipher, N, length(PartOfText)))} || N <- lists:seq(2, length(Cipher) - length(PartOfText) + 1)],
+    {N, KeyShifted} = lists:foldr(fun({N, X}, {M, Y}) -> if length(X) < length(Y) -> {N, X}; true -> {M, Y} end end, {0, KeyFirst}, Keys),
+    Shift = length(KeyShifted) - (N rem length(KeyShifted)),
+    Key = lists:concat([lists:sublist(KeyShifted, Shift + 1, length(KeyShifted)), lists:sublist(KeyShifted, Shift)]),
     decrypt(Cipher, Key).
